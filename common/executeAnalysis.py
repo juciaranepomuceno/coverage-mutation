a531f17de003b843e90a3e34dc95fbb2b1f1391c
juciaranepomuceno <juciara.nep@gmail.com>
2018-02-21 11:36:35 +0100

    ignore IDE files

--- common/executeAnalysis.py
+++ common/executeAnalysis.py
@@ -1,23 +1,27 @@
 import os
 import countUnit
 import findMutationReport
+import sheetMap
 
 
+print ("**************** COVERAGE ANALYSIS SUPPORTE **************************** \n"
+       "*Please, be sure that your module is in C:/dev/repository/[your module]* \n"
+       "************************************************************************ \n")
 
 module = input("Module: ")
 path = "C:/dev/repository/"+module+"/layers"
 
-print (path)
 dirUnit= countUnit.CountUnit()
 dirMutation = findMutationReport.FindReport()
+print(sheetMap.sheetId(module))
+
 
 if os.path.exists(path):
     dirUnit.findUnitReport('frame-summary.html',path)
     dirMutation.findMutationReport('mutations.csv',path)
 
-    
 else:
-    print("Please verify the path...")
+    print("Please verify the path of your module...")
 
 
 
